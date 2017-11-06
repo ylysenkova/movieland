a0d0616555941d3598ff3885b2b7e20781d3482d
Yana <yana.lysenkova@gmail.com>
2017-11-06 22:20:38 +0200

    task b-3 commited

--- src/main/java/com/ylysenkova/movieland/service/impl/MovieServiceImpl.java
+++ src/main/java/com/ylysenkova/movieland/service/impl/MovieServiceImpl.java
@@ -2,14 +2,14 @@ package com.ylysenkova.movieland.service.impl;
 
 import com.ylysenkova.movieland.dao.jdbc.impl.JdbcMovieDaoImpl;
 import com.ylysenkova.movieland.model.Movie;
-import com.ylysenkova.movieland.service.serviceInterface.MovieServiceInterface;
+import com.ylysenkova.movieland.service.serviceInterface.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class MovieServiceImpl implements MovieServiceInterface{
+public class MovieServiceImpl implements MovieService {
 
     @Autowired
     JdbcMovieDaoImpl jdbcMovieDaoImpl;
--- src/test/java/com/ylysenkova/movieland/dao/jdbc/impl/JdbcMovieDaoImplTest.java
+++ src/test/java/com/ylysenkova/movieland/dao/jdbc/impl/JdbcMovieDaoImplTest.java
@@ -3,18 +3,14 @@ package com.ylysenkova.movieland.dao.jdbc.impl;
 import com.ylysenkova.movieland.model.Movie;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/spring/spring-test-config.xml"})
 public class JdbcMovieDaoImplTest {
