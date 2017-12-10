9125cce396e27eb396ad1be5debfbfa2f4c71ff9
Yana <yana.lysenkova@gmail.com>
2017-12-10 22:01:36 +0200

    [X-2] Cache for movies. Retrieve final access modifier to Genre entity

--- src/main/java/com/ylysenkova/movieland/model/Genre.java
+++ src/main/java/com/ylysenkova/movieland/model/Genre.java
@@ -2,11 +2,8 @@ package com.ylysenkova.movieland.model;
 
 
 public class Genre {
-    private int id;
-    private String name;
-
-    public Genre() {
-    }
+    private final int id;
+    private final String name;
 
     public Genre(int id, String name) {
         this.id = id;
